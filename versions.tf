b48dddf8f46ba9b95697e99595d402a9d3611ac5
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2025-02-26 14:53:35 +0100

    fix: update files & cleanup (#1)
    * fix: update files & cleanup
    
    * Update auto_assignee.yml
    
    * Update auto_assignee.yml
    
    * Update auto_assignee.yml
    
    * Update auto_assignee.yml
    
    * Update auto_assignee.yml
    
    ---------
    
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>
    Co-authored-by: Anmol Nagpal <4303310+anmolnagpal@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,12 +1,17 @@
 # ------------------------------------------------------------------------------
 # Versions
 # ------------------------------------------------------------------------------
+# Terraform version
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.10.5"
+}
+
+terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.1.0"
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      configuration_aliases = [azurerm.main_sub, azurerm.dns_sub]
+      version               = ">=3.85.0"
     }
   }
 }
